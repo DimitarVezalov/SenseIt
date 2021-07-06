6c7c44ecd8bc7bc09d5e6f7ba1195d4ea06b7fdf
Dimitar Vezalov <wopopo13@gmail.com>
2021-07-07 01:25:55 +0300

    Add dropdown

---
+++ Services/SenseIt.Services.Data/Admin/Models/AdminProductDetailsViewModel.cs
@@ -0,0 +1,17 @@
+﻿namespace SenseIt.Services.Data.Admin.Models
+{
+    public class AdminProductDetailsViewModel
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public string Category { get; set; }
+
+        public string ImageUrl { get; set; }
+
+        public decimal Price { get; set; }
+    }
+}
---
+++ Services/SenseIt.Services.Data/Admin/Models/AdminProductUpdateModel.cs
@@ -0,0 +1,27 @@
+﻿namespace SenseIt.Services.Data.Admin.Models
+{
+    using System;
+    using System.ComponentModel.DataAnnotations;
+
+    public class AdminProductUpdateModel
+    {
+        [Required]
+        [MinLength(5)]
+        [MaxLength(100)]
+        public string Name { get; set; }
+
+        [Required]
+        [MinLength(5)]
+        [MaxLength(800)]
+        public string Description { get; set; }
+
+        [Required]
+        public string Category { get; set; }
+
+        [Required]
+        public string ImageUrl { get; set; }
+
+        [Range(0.1, 1000.00)]
+        public decimal Price { get; set; }
+    }
+}
