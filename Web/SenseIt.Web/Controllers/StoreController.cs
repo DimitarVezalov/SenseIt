7f43dd0fc0f77e8564d1fee9f3eb3035261a65f0
Dimitar Vezalov <wopopo13@gmail.com>
2021-07-21 18:11:05 +0300

    Change Home/Index Page Added Fontawesome Library

---
+++ Web/SenseIt.Web/Controllers/StoreController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SenseIt.Web.Controllers
+{
+    public class StoreController : Controller
+    {
+        public IActionResult ProductsAll()
+        {
+            return this.View();
+        }
+    }
+}
