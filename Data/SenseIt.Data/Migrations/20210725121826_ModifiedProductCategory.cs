0dcea7db918c5ba5772eb312718b61145b2d484f
Dimitar Vezalov <wopopo13@gmail.com>
2021-07-25 17:27:27 +0300

    Refactoring Admin Area / AppServiceCategories

---
+++ Data/SenseIt.Data/Migrations/20210725121826_ModifiedProductCategory.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SenseIt.Data.Migrations
+{
+    public partial class ModifiedProductCategory : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "ServiceCategories",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ImageUrl",
+                table: "ServiceCategories",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "ServiceCategories");
+
+            migrationBuilder.DropColumn(
+                name: "ImageUrl",
+                table: "ServiceCategories");
+        }
+    }
+}
