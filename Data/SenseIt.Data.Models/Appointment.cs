43e093fa17ee4bcc42a084dec60acf064b28ef24
Dimitar Vezalov <wopopo13@gmail.com>
2021-08-02 23:32:58 +0300

    Added Rating Stars Tooltips / Added Appointment DbTable

---
+++ Data/SenseIt.Data.Models/Appointment.cs
@@ -0,0 +1,22 @@
+﻿namespace SenseIt.Data.Models
+{
+    using System;
+    using System.ComponentModel.DataAnnotations.Schema;
+
+    using SenseIt.Data.Common.Models;
+
+    public class Appointment : BaseDeletableModel<int>
+    {
+        [ForeignKey(nameof(User))]
+        public string UserId { get; set; }
+
+        public ApplicationUser User { get; set; }
+
+        [ForeignKey(nameof(Service))]
+        public int ServiceId { get; set; }
+
+        public Service Service { get; set; }
+
+        public DateTime StartDate { get; set; }
+    }
+}
--- Data/SenseIt.Data/ApplicationDbContext.cs
+++ Data/SenseIt.Data/ApplicationDbContext.cs
@@ -33,6 +33,8 @@
 
         public DbSet<Review> Reviews { get; set; }
 
+        public DbSet<Appointment> Appointments { get; set; }
+
         public override int SaveChanges() => this.SaveChanges(true);
 
         public override int SaveChanges(bool acceptAllChangesOnSuccess)
